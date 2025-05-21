b017a90e0923d68d7088ef77e65926f5700f957d
fedetempone <fedetempone5@gmail.com>
2025-05-21 20:44:50 -0300

    trying to fix cors error

--- backend/Server.js
+++ backend/Server.js
@@ -3,6 +3,9 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { getProducts } from './controllers/productController.js';
+import cors from 'cors';
+
+const allowedOrigins = ['https://ecommerce-legotempone.onrender.com'];
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -12,6 +15,18 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // Permite requests sin origin como Postman o curl
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'El CORS policy no permite este origen: ' + origin;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+}));
 
 // Conexión a MongoDB
 mongoose
